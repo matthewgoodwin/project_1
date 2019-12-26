ede998b7c393ad74cf8bc81a1326d92ac70f9815
matthew goodwin <matthewjgoodwin@gmail.com>
2019-12-26 15:39:50 +0900

    altered the first app file: this file was created before I began organizing files by lesson and date.. this file is not a junk file and has been renamed -random_snippets-

---
+++ rondom_snippets.js
@@ -0,0 +1,124 @@
+//RANDOM JUNK FILE
+
+
+// console.log('hello');
+// console.log([1,2,3]);
+
+// //constant array
+// const val1 = [1, 2, 3];
+// const food = ["sandwich","cookies","pasta","duck"];
+// const person = {
+//   fName: "matthew",
+//   lName: "goodwin",
+//   age: 30,
+//   hobby: ['cooking','coding','comics', 'gym'],
+//   address: {
+//     city: "palo alto",
+//     state: "california",
+//   },
+// };
+
+// let number1 = 50;
+// let number2 = 100;
+
+// let val;
+// val = number1 + number2;
+// val = number1 * number2;
+// //add to end
+// val1.push(4);
+// //add to front
+// val1.unshift(0);
+// //take range with splice ("start", "end")
+// //starts at 1 spot and ends at 1 spot
+// val1.splice(1,1);
+
+// //alphabetica order
+// food.sort();
+// console.log(val1);
+// console.log(val);
+// console.log(food);
+
+//-----------------------------------
+
+// const users = [
+//   {id: 33, name: 'steve' , age:20 },
+//   {id: 44, name: 'jessica', age:21 },
+//   {id: 55, name: 'matt', age:22 }
+// ];
+
+
+// const users2 = {
+//   name: 'steve',
+//   age: 46,
+//   color: 'yellow'
+// };
+
+
+// const vehicles = ['car','truck','plane','train'];
+// let check
+// const human = {
+//   hair : 'red',
+//   eye: 'green',
+//   skin: 'brown'
+// };
+
+// vehicles.push('bus','pizza');
+
+// check = Array.isArray(vehicles);
+
+// console.log(vehicles);
+// vehicles.pop();
+
+
+
+
+// console.log(check);
+// console.log(typeof vehicles);
+// //arrays are objects
+// console.log(typeof human);
+// console.log('human hair color is' + ' ' + human.hair);
+// console.log(vehicles);
+
+// let id = '100';
+
+// if(typeof id == 'number'){
+//   console.log('id '+ id + ' ' + 'is a number');}
+//   else {
+//     console.log('id ' + id + ' ' + ' is a string')
+//   };
+
+
+// //works for arrays
+// console.log('array methods = forEach');
+// for(i = 0; i < vehicles.length; i++){
+//   console.log(vehicles[i]);
+// };
+
+// //this is the same as above works for arrays
+// console.log('array methods  = forEach');
+// vehicles.forEach(function(vehicle){
+//   console.log(vehicle);
+// });
+
+// console.log('forEach array method');
+// users.forEach(function(user){
+//   console.log(user);
+// });
+// console.log('map method for array of objects');
+// const ids = users.map(function(user){
+//   return user.id
+// });
+// console.log(ids);
+
+
+// console.log('for X in Y = object methods .. gives keys');
+// for(let x in users2 ){
+// console.log(x + ' - is a key');
+// console.log(`${x} : ${users2[x]}`);
+// };
+
+
+
+
+
+
